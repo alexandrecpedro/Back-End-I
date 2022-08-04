a7d75cefa458d72ee6c2ba48cfdd578e294b77c1
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-08-03 22:20:50 -0300

    Class 05 - Proxy pattern

--- Aula 05 - Proxy pattern/class_05_proxy_pattern/src/Vaccination/model/PessoaTest.java
+++ Aula 05 - Proxy pattern/class_05_proxy_pattern/src/Vaccination/model/PessoaTest.java
@@ -11,10 +11,10 @@ class PessoaTest {
 
     @Test
     public void vac() {
-        Pessoa p1 = new Pessoa("Christh", "Teixeira", "1122334455", "Pfizer", dataVac);
+        Pessoa pessoa = new Pessoa("Christh", "Teixeira", "1122334455", "Pfizer", dataVac);
         Vacinar vacinar = new ServicoVacinarProxy();
-        vacinar.vacinarPessoa(p1.getRg(), p1.getDataVacina(), p1.getNomeVacina());
-        System.out.println(p1.toString());
+        vacinar.vacinarPessoa(pessoa.getRg(), pessoa.getDataVacina(), pessoa.getNomeVacina());
+        System.out.println(pessoa.toString());
     }
 
 }
