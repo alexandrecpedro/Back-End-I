36609e24a093279689efbe1a8e69f366797cd8db
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-08-06 23:09:16 -0300

    Class 10 - Logging (Trace and Debug)

--- Aula 10 - Logging/class_10_logging/src/MathCalc/Main.java
+++ Aula 10 - Logging/class_10_logging/src/MathCalc/Main.java
@@ -4,40 +4,54 @@ import MathCalc.model.ListaNumeros;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
+
 public class Main {
 
-    private static final Logger logger = Logger.getLogger(Main.class);
+    private static final Logger logger = Logger.getLogger(ListaNumeros.class);
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // Configuring log4j.properties (Alternative)
         BasicConfigurator.configure();
 
-        ListaNumeros lista1 = new ListaNumeros();
-        lista1.addNumero(2);
-        lista1.addNumero(3);
-        lista1.addNumero(5);
-        lista1.addNumero(8);
-        lista1.addNumero(23);
-        lista1.addNumero(1);
-        lista1.addNumero(1);
-        lista1.addNumero(1);
-        lista1.addNumero(1);
-        lista1.addNumero(1);
-        lista1.addNumero(1);
-        lista1.qtdNumeros();
-        lista1.media();
+        ArrayList<Integer> lista1 = new ArrayList();
+        lista1.add(1);
+        lista1.add(5);
+        lista1.add(9);
+        lista1.add(10);
+        lista1.add(111);
+        lista1.add(49);
 
-        ListaNumeros lista2 = new ListaNumeros();
-        lista2.addNumero(3);
-        lista2.addNumero(3);
-        lista2.addNumero(3);
-        lista2.addNumero(3);
-        lista2.addNumero(3);
-        lista2.addNumero(3);
-        lista2.qtdNumeros();
-        lista2.media();
+        ArrayList<Integer> lista2 = new ArrayList();
+        lista2.add(5);
+        lista2.add(6);
+        lista2.add(-1);
+        lista2.add(40);
+        lista2.add(13);
+        lista2.add(9);
+        lista2.add(40);
+        lista2.add(4);
+        lista2.add(7);
+        lista2.add(20);
+        lista2.add(30);
 
-        ListaNumeros lista3 = new ListaNumeros();
-        lista3.qtdNumeros();
+        ArrayList<Integer> lista3 = new ArrayList();
+
+        ListaNumeros listaNumeros1 = new ListaNumeros(lista1);
+        ListaNumeros listaNumeros2 = new ListaNumeros(lista2);
+        ListaNumeros listaNumeros3 = new ListaNumeros(lista3);
+
+        try {
+            System.out.println("Lista 1");
+            listaNumeros1.getInfo();
+            System.out.println();
+            System.out.println("Lista 2");
+            listaNumeros2.getInfo();
+            System.out.println();
+            System.out.println("Lista 3");
+            listaNumeros3.getInfo();
+        } catch (Exception e) {
+            logger.error(e);
+        }
     }
 }
--- Aula 10 - Logging/class_10_logging/src/MathCalc/model/ListaNumeros.java
+++ Aula 10 - Logging/class_10_logging/src/MathCalc/model/ListaNumeros.java
@@ -8,35 +8,67 @@ import java.util.List;
 
 public class ListaNumeros {
     /** Attributes **/
-    private static final DecimalFormat df = new DecimalFormat("0.00");
+    private static final DecimalFormat df = new DecimalFormat("#.##");
 
     private static final Logger logger = Logger.getLogger(ListaNumeros.class);
     private List<Integer> listaInteiros = new ArrayList<>();
 
-    /** Getter **/
-    public List<Integer> getListaInteiros() {
-        return listaInteiros;
+    /** Constructor **/
+    public ListaNumeros(List<Integer> listaInteiros) {
+        this.listaInteiros = listaInteiros;
     }
 
     /** Methods **/
-    public void addNumero(int numero) {
-        listaInteiros.add(numero);
-    }
+    // Media of numbers
+    public double getMedia() {
+        double media = 0;
 
-    public void qtdNumeros() throws Exception {
-        if (listaInteiros.size() > 10) {
-            logger.info("A lista tem mais de 10 itens!");
-        } else if (listaInteiros.size() > 5) {
-            logger.info("A lista tem mais de 5 itens!");
-        } else if (listaInteiros.size() == 0) {
-            logger.error("A lista está vazia!");
-            throw new Exception();
+        for (int numero: listaInteiros) {
+            media += numero;
         }
+
+        return media / listaInteiros.size();
+        // return listaInteiros.stream().reduce(0, Integer::sum) / listaInteiros.size();
     }
 
-    public void media() {
-        double media = listaInteiros.stream().reduce(0, Integer::sum ) / listaInteiros.size();
-        logger.info("A média da lista é: " + df.format(media));
+    // Max number
+    public int getMax() {
+        int max = listaInteiros.get(0);
+
+        for (int numero: listaInteiros) {
+            if (! (numero < max)) {
+                max = numero;
+            }
+        }
+        return max;
+    }
+
+    // Min number
+    public int getMin() {
+        int min = listaInteiros.get(0);
+
+        for (int numero: listaInteiros) {
+            if (!(numero > min)) {
+                min = numero;
+            }
+        }
+        return min;
+    }
+
+    // List of numbers info
+    public void getInfo() throws Exception {
+        if (listaInteiros.size() == 0) {
+            throw new Exception("A lista está vazia");
+        } else if (listaInteiros.size() > 5 ) {
+            logger.info("O comprimento da lista é maior que 5");
+        } else if (listaInteiros.size() > 10) {
+            logger.info("O comprimento da lista é maior que 10");
+        }
+
+        logger.info("A média da lista é: " + df.format(getMedia()));
+        // logger.info("A média da lista é: " + getMedia());
+        logger.info("O maior valor da lista é: " + getMax());
+        logger.info("O menor valor da lista é: " + getMin());
     }
 
 }
