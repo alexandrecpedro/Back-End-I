e0170e46d766fd36300ac8d6beff6d2a26c57cb6
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-08-16 12:29:48 -0300

    Class 01 - JUnit

--- Aula 01 - JUnit/class_01_JUnit/src/geometric_figures/ShapeTest.java
+++ Aula 01 - JUnit/class_01_JUnit/src/geometric_figures/ShapeTest.java
@@ -6,9 +6,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ShapeTest {
+    /** Attributes **/
+    Shape circle, square;
+
+    /** Methods **/
+    @BeforeEach
+    void doBefore() {
+        circle = new Circle(1.5);
+        square = new Square(2);
+    }
+
+    /** Tests **/
     @Test
     public void shouldReturnPositeResponseCircle(){
-        Shape circle = new Circle(1.5);
         assertEquals(9.42477796076938, circle.calculatePerimeter());
         System.out.println(circle.toString());
         System.out.printf("perimeter = " + "%.2f", circle.calculatePerimeter());
@@ -16,7 +26,6 @@ class ShapeTest {
 
     @Test
     public void shouldReturnPositeResponseSquare(){
-        Shape square = new Square(2);
         assertEquals(8 , square.calculatePerimeter());
         System.out.println("\n\n" + square.toString());
         System.out.println("perimeter = " + square.calculatePerimeter());
