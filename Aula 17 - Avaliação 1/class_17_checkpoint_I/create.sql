31a66352014617f80e65ab78be8a38ca78264b9e
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-08-16 22:58:05 -0300

    Class 17 - JUnit, Log4J, Database and DAO (Data Access Object)

---
+++ Aula 17 - Avaliação 1/class_17_checkpoint_I/create.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS pacientes;
+DROP TABLE IF EXISTS enderecos;
+CREATE TABLE IF NOT EXISTS enderecos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    rua VARCHAR(64),
+    numero VARCHAR(8),
+    cidade VARCHAR(32),
+    bairro VARCHAR(32)
+);
+
+
+CREATE TABLE IF NOT EXISTS pacientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(64),
+    sobrenome VARCHAR(64),
+    rg VARCHAR(20),
+    dataCadastro DATE,
+    idEndereco INT
+);
+ALTER TABLE pacientes ADD FOREIGN KEY (idEndereco) REFERENCES enderecos(id);
