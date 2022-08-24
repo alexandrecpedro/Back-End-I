bffef10aebd5841dd5144a0194972282e2331472
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-08-23 21:13:33 -0300

    Class 21 - Spring Boot Introduction

---
+++ Aula 21 - Introdução ao Spring Boot/Animals/src/main/java/com/example/animals/AnimalsApplication.java
@@ -0,0 +1,21 @@
+package com.example.animals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AnimalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalsApplication.class, args);
+    }
+
+    @GetMapping
+    public String Hello() {
+        return "Hello";
+    }
+
+}
